593b3e70875dc73b957ff625f2ab8d79335368ae
CoolPlayLin <CoolPlayLin@qq.com>
2023-05-06 21:02:12 +0800

    bugfix

--- __test__/main.test.ts
+++ __test__/main.test.ts
@@ -1,8 +1,5 @@
 import { output_tags } from "../src/until";
-import { main } from "./tagger.test";
 
 console.log("Output tag testing.....");
 console.log(await output_tags("", "Tagger", "CoolPlayLin"));
 console.log("Complete");
-console.log("Testing tagger......");
-await main();
--- __test__/tagger.test.ts
+++
@@ -1,9 +0,0 @@
-import { context } from "@actions/github";
-import { Octokit } from "@octokit/rest";
-import * as t from "../src/typing";
-import * as fs from "fs";
-import * as until from "../src/until";
-
-var run = ["issue", "pull_request"];
-
-export async function main(): Promise<void> {}
