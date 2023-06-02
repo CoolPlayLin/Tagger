deda584ad505be8386f172b227e27809d471f62b
CoolPlayLin <CoolPlayLin@qq.com>
2023-06-02 22:35:44 +0800

    Update

--- src/envs/env.ts
+++ src/envs/env.ts
@@ -2,14 +2,22 @@ import * as core from "@actions/core";
 import { Octokit } from "@octokit/rest";
 import * as t from "../types";
 
-export const inputs : t.inputs = {
+export var github: Octokit
+export var inputs: t.inputs | any
+
+try{inputs = {
     token: core.getInput("repo-token"),
     path: core.getInput("path"),
     default_tag: core.getInput("default-tag"),
     debug: core.getBooleanInput("debug"),
     removeAllTags: core.getBooleanInput("removeAllTags"),
-  };
+}}catch {
+  inputs = undefined}
 
-export const github = new Octokit({
+try {
+  github = new Octokit({
     auth: inputs.token
 })
+} catch{
+  github = new Octokit
+}
