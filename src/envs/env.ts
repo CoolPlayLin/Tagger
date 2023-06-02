4ee72207f54b43606acb0b87b5059b7e4236bc5d
CoolPlayLin <coolplaylin@qq.com>
2023-06-02 22:38:39 +0800

    Update (#25)
    * Update
    
    * Update

---
+++ src/envs/env.ts
@@ -0,0 +1,23 @@
+import * as core from "@actions/core";
+import { Octokit } from "@octokit/rest";
+import * as t from "../types";
+
+export var github: Octokit
+export var inputs: t.inputs | any
+
+try{inputs = {
+    token: core.getInput("repo-token"),
+    path: core.getInput("path"),
+    default_tag: core.getInput("default-tag"),
+    debug: core.getBooleanInput("debug"),
+    removeAllTags: core.getBooleanInput("removeAllTags"),
+}}catch {
+  inputs = undefined}
+
+try {
+  github = new Octokit({
+    auth: inputs.token
+})
+} catch{
+  github = new Octokit
+}
---
+++ src/envs/index.ts
@@ -0,0 +1,8 @@
+import { context } from "@actions/github";
+
+export const envs = {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    number: context.payload.issue?.number || context.payload.pull_request?.number || -1,
+    title: context.payload.issue?.title,
+}
