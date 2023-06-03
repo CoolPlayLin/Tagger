728748158e1c37fa29834b320005a51c6296e33d
CoolPlayLin <coolplaylin@qq.com>
2023-06-03 18:55:06 +0800

    [0.0.5] bugfix (#30)
    * Update
    
    * Update
    
    * Update

--- src/tagger.ts
+++ src/tagger.ts
@@ -3,43 +3,48 @@ import { github, inputs } from "./envs/env";
 import { envs } from "./envs";
 
 export function main(): void {
-  // Verify env
-  if (!envs.title || envs.number == -1 || inputs.RUNTIME_ERROR) {
+  if (inputs.RUNTIME_ERROR) {
     const error = Error(
       "No information about pull requests and issues is currently available"
     );
     throw error;
   }
-  // Get base information
+  let option = inputs.removeAllTags ? "removeAllTags" : ""
+  until.preparation(envs.repo, envs.owner, envs.number, option)
+  .finally(() => {
+    tagger(envs.repo, envs.owner, {
+      title: envs.title,
+      default_tag: inputs.default_tag,
+      issue_number: envs.number
+    })
+  })
+}
+
+function tagger(repo: string, owner: string, options: {title: string, default_tag: string, issue_number: number}): void {
   until
     .get_template(inputs.path)
     .then((template) =>
-      until.verify_template(template, inputs.token, {
-        repo: envs.repo,
-        owner: envs.owner,
+      until.verify_template(template, {
+        repo: repo,
+        owner: owner,
       })
     )
-    .then((template) => until.tag(template, envs.title, inputs.default_tag))
+    .then((template) => until.tag(template, options.title, options.default_tag))
     .then((tags) => {
-      if (inputs.removeAllTags) {
-        until.preparation(envs.repo, envs.owner, envs.number, "removeAllTags");
-      }
+      github.issues.addLabels({
+        repo: repo,
+        owner: owner,
+        issue_number: options.issue_number,
+        labels: tags,
+      }).then(res => {
+        until.logger("event", false, `Tag-adding request has been sent`)
+        if (res.status === 200){
+          until.logger("event", false, "Tag-adding request has been succeeded")
+        } else {
+          until.logger("event", false, `Unknown Status Code: ${res.status}`)
+        }
 
-      return tags;
-    })
-    .then((tags) => {
-      if (inputs.removeAllTags) {
-        github.issues.removeAllLabels({
-          repo: envs.repo,
-          owner: envs.owner,
-          issue_number: envs.number,
-        });
-      }
-      until.add_tags(inputs.token, {
-        owner: envs.owner,
-        repo: envs.repo,
-        number: envs.number,
-        tags: tags,
-      });
+        until.logger("event", false, "Thanks for using")
+      })
     });
 }
