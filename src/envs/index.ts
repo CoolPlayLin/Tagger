a9fc26e3a895e84b2263ac8c06a843dc73fbd9e1
coolplaylinbot <130831920+coolplaylinbot@users.noreply.github.com>
2023-06-10 12:29:52 +0000

    [Automated] Format All of the file

--- src/envs/index.ts
+++ src/envs/index.ts
@@ -4,17 +4,17 @@ import { get_input } from "../until";
 import { input } from "../types";
 
 export const inputs: input = {
-  token: get_input("repo-token", "string") as string || "",
-  path: get_input("path", "string") as string || "",
-  default_tag: get_input("default-tag", "string") as string || "",
-  debug: get_input("debug", "boolean") as boolean || false,
-  removeAllTags: get_input("removeAllTags", "boolean") as boolean || false,
+  token: (get_input("repo-token", "string") as string) || "",
+  path: (get_input("path", "string") as string) || "",
+  default_tag: (get_input("default-tag", "string") as string) || "",
+  debug: (get_input("debug", "boolean") as boolean) || false,
+  removeAllTags: (get_input("removeAllTags", "boolean") as boolean) || false,
   RUNTIME_ERROR: get_input("RUNTIME", "RUNTIME") as boolean,
-}
+};
 
 export const github = new Octokit({
   auth: inputs.token,
-});;
+});
 
 export const envs = {
   owner: context.repo.owner,
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
 import { main } from "./tagger";
 
-
 main();
